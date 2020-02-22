524682abbaf9f577d2944a0577f102d4a485eaa0
DanielAM31 <55641988+DanielAM31@users.noreply.github.com>
2020-02-22 03:20:58 -0500

    Add files via upload

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -4,62 +4,43 @@ import java.awt.GridLayout;
 import java.awt.event.*;
 
 public class Interfaz extends JFrame implements ActionListener {
-	JButton boton1;
-	JButton boton2;
-	JButton boton3;
-	JButton boton4;
+	
+	int filabotones;
+	int cantidadbotones; 
+	JButton matrizbotones[];
 	JPanel panel;
-	ImageIcon iconobtn;
+	String Iconurl1 = "src/img/phone.png";
+	String Iconurl2 = "src/img/gold.png";
+	ImageIcon iconobtn1 = new ImageIcon(Iconurl1);
+	ImageIcon iconobtn2 = new ImageIcon(Iconurl2);
 	
-	
-	public Interfaz() {
+	public Interfaz(int fila) {
 		
-		/*Creación del Boton1 con caracteristicas*/
+		/*Creación de los Botones y el panel*/
 		
-		//setLayout(null);
-		iconobtn = new ImageIcon("src/img/phone.png");
-		boton1 = new JButton(iconobtn);
-		boton2 = new JButton(iconobtn);
-		boton3 = new JButton(iconobtn);
-		boton4 = new JButton(iconobtn);
+		this.filabotones = fila;
+		this.cantidadbotones = this.filabotones * this.filabotones;
+		this.matrizbotones = new JButton[this.cantidadbotones];
+		this.panel = new JPanel();
+		this.panel.setLayout(new GridLayout(this.filabotones, this.filabotones));
 		
-		//boton1.setBounds(300, 250, 100, 30);
-		//add(boton1);
-		boton1.addActionListener(this);
-		boton2.addActionListener(this);
-		boton3.addActionListener(this);
-		boton4.addActionListener(this);
-		
-		/*Creacion del Panel y contenido*/
-		
-		panel = new JPanel();
-		panel.setLayout(new GridLayout(2,2));
-		panel.add(boton1);
-		panel.add(boton2);
-		panel.add(boton3);
-		panel.add(boton4);
-		add(panel);
-		
-		
+		for(int i=0; i < this.cantidadbotones; i++) {
+			this.matrizbotones[i] = new JButton(this.iconobtn1);
+			this.matrizbotones[i].addActionListener(this);
+			this.panel.add(this.matrizbotones[i]);
+		}
+				
+		add(this.panel);
+				
 	}
 	
 	public void actionPerformed(ActionEvent c) {
 		
-		iconobtn = new ImageIcon("src/img/gold.png");
-		if(c.getSource() == boton1) {
-			boton1.setIcon(iconobtn);
-		}
-		
-		if(c.getSource() == boton2) {
-			boton2.setIcon(iconobtn);
-		}
-		
-		if(c.getSource() == boton3) {
-			boton3.setIcon(iconobtn);
-		}
-		
-		if(c.getSource() == boton4) {
-			boton4.setIcon(iconobtn);
+		for(int i=0; i < this.cantidadbotones; i++) {
+			if(c.getSource() == this.matrizbotones[i]) {
+				this.matrizbotones[i].setIcon(this.iconobtn2);
+				try{Thread.sleep(100);}catch(InterruptedException ie){}
+			}
 		}
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 
 public class Main {
 	public static void main(String arg[]) {
-		Interfaz form = new Interfaz();
-		form.setBounds(0, 0, 450, 350);
+		int fila = 8;
+		Interfaz form = new Interfaz(fila);
+		form.setBounds(0, 0, fila * 50 + 10, fila * 50 + 60);
 		form.setVisible(true);
 	}
 }
